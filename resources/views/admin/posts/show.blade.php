84a5f10dfd7556289ec56c660e1a17cef3394d8c
Tetyana <tsergata@gmail.com>
2020-04-01 16:56:56 +0200

    Admin show completata

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -1,1 +1,27 @@
-@dd($posts);
+{{-- @dd($posts); --}}
+@extends('layouts.app')
+@section('content')
+  <div class="container border">
+    <table class="table">
+      <thead class="thead-dark">
+        <tr>
+          <th>ID</th>
+          <th>USER ID</th>
+          <th>TITLE</th>
+          <th>CREATED AT</th>
+          <th>UPDATED AT</th>
+          <th colspan="3"></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{{$post->id}}</td>
+          <td>{{$post->user_id}}</td>
+          <td>{{$post->title}}</td>
+          <td>{{$post->created_at}}</td>
+          <td>{{$post->updated_at}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+@endsection
